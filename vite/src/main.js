cf24c5d15fd99e23d5a52f1d71dabb45f5fa7cb6
sjl-03 <shenjunlu03@gmail.com>
2025-05-28 21:15:18 +0100

    demo complete

--- vite/src/main.js
+++ vite/src/main.js
@@ -3,12 +3,15 @@ import {OrbitControls} from "three/addons/controls/OrbitControls.js";
 
 const canvas = document.getElementById('canvas');
 
+// 1. Scene
 const scene = new THREE.Scene();
 scene.background = new THREE.Color('#F0F0F0');
 
+// 2. Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 
+// 3. Object
 const geometry = new THREE.DodecahedronGeometry();
 const material = new THREE.MeshLambertMaterial({color: '#468585', emissive: '#468585'});
 const dodecahedron = new THREE.Mesh(geometry, material);
@@ -21,10 +24,12 @@ box.position.y = -1.5;
 scene.add(dodecahedron);
 scene.add(box);
 
+// 4. Lighting
 const light = new THREE.SpotLight(0x006769, 100);
 light.position.set(1,1,1);
 scene.add(light);
 
+// 5. Renderer
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
