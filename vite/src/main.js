0ddf69150b490e72bccc66c496bef26421405204
sjl-03 <shenjunlu03@gmail.com>
2025-05-28 21:10:08 +0100

    add control and orbit

--- vite/src/main.js
+++ vite/src/main.js
@@ -14,7 +14,7 @@ const material = new THREE.MeshLambertMaterial({color: '#468585', emissive: '#46
 const dodecahedron = new THREE.Mesh(geometry, material);
 
 const boxGeometry = new THREE.BoxGeometry(2, 0.1, 2);
-const boxMaterial = new THREE.MeshBasicMaterial({color: '#B4B4B3'});
+const boxMaterial = new THREE.MeshStandardMaterial({color: '#B4B4B3', emissive: '#B4B4B3'});
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 box.position.y = -1.5;
 
@@ -49,5 +49,12 @@ function animate(){
   controls.update();
   renderer.render(scene,camera);
 }
+
+// 8. Handle window resizing
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+})
 
 animate();
