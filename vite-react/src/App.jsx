7813d2fdd9a327c625089d6050a64fd44feb0d85
sjl-03 <shenjunlu03@gmail.com>
2025-06-04 17:37:33 +0100

    vite -w react tutorial complete

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,5 +1,5 @@
 import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Sparkles } from "@react-three/drei";
 import { useRef } from "react";
 
 const RotatingCube = () => {
@@ -16,6 +16,8 @@ const RotatingCube = () => {
     <mesh ref={meshRef}>
       <cylinderGeometry args={[1,1,1]} />
       <meshLambertMaterial color="#468585" emissive="#468585" />
+
+      <Sparkles count = {100} scale={1} size={6} speed={0.002} noise={0.2} color="orange"/>
     </mesh>
   )
 }
