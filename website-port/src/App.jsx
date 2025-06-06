74f97d5a89ee94f085bf8ab84452b487711443c6
sjl-03 <shenjunlu03@gmail.com>
2025-06-06 21:27:48 +0100

    fixed incompatible part to v4 in index

--- website-port/src/App.jsx
+++ website-port/src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import Navbar from './sections/Navbar.jsx'
 
 const App = () => {
   return (
     <main className='maxw-7xl max-auto '>
-      <h1 className='text-2xl text-white underline'> Hello, Three.js!  </h1>
+      <Navbar />
     </main>
   )
 }
---
+++ website-port/src/sections/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
+        <div className="max-w-7xl mx-auto">
+            <div className="flex justify-between items-centre py-5 mx-auto ">
+
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
